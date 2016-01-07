f3d5d2000a41c774b7d541568fb1125fa6ba212f
iPanda <wang_w_t@qq.com>
2016-01-07 17:29:46 +0800

    增加认证逻辑

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var callback = require('./routes/callback');
 
 var app = express(),
 swig = require('swig'),
@@ -28,6 +29,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/callback', callback);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
