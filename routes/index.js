4f94bf371646cbf46ceeaf204c2f768efc5937c1
iPanda <wang_w_t@qq.com>
2016-01-07 23:30:29 +0800

    更改domain为www.xiangrikuimama.com

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', domain:'127.0.01' });
+  res.render('index', { title: 'Express', domain:'www.xiangrikuimama.com' });
 });
 
 module.exports = router;
