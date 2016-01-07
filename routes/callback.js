536205f086f03fc0d96cd7b2818db2b1472f42eb
iPanda <wang_w_t@qq.com>
2016-01-07 23:35:20 +0800

    修改callback逻辑

--- routes/callback.js
+++ routes/callback.js
@@ -3,14 +3,14 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-	res.send('code:'+req.query.code+' type:'+req.query.type);
-//	if(req.query.type) {
-//		if(req.query.type=='jumpauthcode'){
-//			res.render('jumpauthcode', { code: req.query.code });
-//		}
-//	}else{
-//   res.send('no type param, respond with a resource');
-// }
+//	res.send('code:'+req.query.code+' type:'+req.query.type);
+	if(req.query.type) {
+		if(req.query.type=='jumpauthcode'){
+			res.render('jumpauthcode', { code: req.query.code });
+		}
+	}else{
+   res.send('no type param, respond with a resource');
+ }
 });
 
 module.exports = router;
