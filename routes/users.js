ade94894c7a224221d3c80acfb3a9f56e87d38cb
iPanda <wang_w_t@qq.com>
2016-01-17 17:12:47 +0800

    设置cookie

--- routes/users.js
+++ routes/users.js
@@ -4,21 +4,29 @@ var router = express.Router();
 /* GET users listing. */
 router.get('/', function(req, res, next) {
 	var collection = req.db.get('token');
-	collection.find({},{},function(e,docs){
-			if(!e){
-				console.log('type:'+typeof(docs));
-				for(var key in docs) {
-					var v = docs[key];
-					console.log('docs['+key+']:'+v);
-					console.log('type:'+typeof(v));
-					for(var k in v)
-						console.log('v['+k+']:'+v[k]);
-				}
-        res.send('id:'+docs[0]['OpenId']);
-      }else{
-      	res.send('get id error');
-      }
-    });
+	try{
+		collection.find({},{},function(e,docs){
+				if(e){
+					console.log('find open id error!');
+	      	res.send('get user id error');
+	      }else{
+					console.log('type:'+typeof(docs));
+					for(var key in docs) {
+						var v = docs[key];
+						console.log('docs['+key+']:'+v);
+						console.log('type:'+typeof(v));
+						for(var k in v)
+							console.log('v['+k+']:'+v[k]);
+					}
+	        res.send('id:'+docs[0]['OpenId']);
+	      }
+	    });
+		}catch(e){
+			console.log('find user openid: err:'+e.message+' name:'+e.name+' code:'+e.number);
+			res.send('get user id error throw');
+		}finally{
+			console.log('find user openid: finally');
+		}
   //res.send('respond with a resource');
 });
 
